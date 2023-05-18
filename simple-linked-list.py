ef51b6ff0d88564e79f6e7b48b1d7fbc68b34d0c
Eric Mbarushimana <mbarushimanaeric17@gmail.com>
2023-05-18 22:49:49 +0200

    Created PatientList class list

--- simple-linked-list.py
+++ simple-linked-list.py
@@ -42,3 +42,39 @@ class Patient:
         self.name = name
         self.health_status = health_status
         self.next_patient = None
+
+# Defining the Patients list to represent the linked list
+class PatientList:
+    def __init__(self):
+        self.head = None
+    
+    def add_patient(self, id, name, health_status):
+        new_patient = Patient(id, name, health_status)
+        if self.head is None:
+            # If the list is empty, new_patient is added as the list head
+            self.head = new_patient
+        else:
+            # Otherwise, he is added to the end of the list
+            actual_patient = self.head
+            while actual_patient.next_patient is not None:
+                actual_patient = actual_patient.next_patient
+                actual_patient.next_patient = new_patient
+            
+    # Remove patient from the list, receives the id to remove as a parameter
+    def remove_patient(self, id):
+        if self.head is None:
+            # If the list is empty, cancel the operation
+            return
+        elif self.head.id == id:
+            # If the patient to remove is the head, move the pointer to the next patient
+            self.head = self.head.next_patient
+            return
+        else:
+            # Otherwise, iterate over the list until we find the id we're looking for
+            actual_patient = self.head
+            while actual_patient.next_patient is not None:
+                if actual_patient.next_patient.id == id:
+                    # If found, update the the previous patient pointer to the next_patient
+                    actual_patient.next_patient = actual_patient.next_patient.next_patient
+                    return
+                actual_patient = actual_patient.next_patient
